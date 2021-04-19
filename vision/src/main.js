62bf6b4d6233f8e0e315a446da84d15e8afed1a7
GuYun-D <1946306647@qq.com>
2021-04-19 15:11:37 +0800

    echarts和axios的全局挂载

--- vision/src/main.js
+++ vision/src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+/**
+ * 配置axios
+ */
+import axios from "axios"
+// 配置请求接口基准路径
+axios.defaults.baseURL = "http://localhost:8000/api"
+// 将axios挂载到vue的原型对象上
+Vue.prototype.$http = axios
+
+// 将全局echarts对象挂载到vue原型对象上
+// 在别的组件中就能通过$.echarts使用了
+Vue.prototype.$echarts = window.echarts
+
 Vue.config.productionTip = false
 
 new Vue({
