b46b4580e5e67a29e3b1e644285b03ac0c7410f9
GuYun-D <1946306647@qq.com>
2021-04-19 14:34:28 +0800

    跨域配置

--- server/middleware/koa_respones_header.js
+++ server/middleware/koa_respones_header.js
@@ -2,5 +2,8 @@
 module.exports = async (ctx, next) => {
     const contentType = "application/json;charset=UTF-8"
     ctx.set("Content-Type", contentType)
+    // 设置响应头，允许跨域
+    ctx.set("Access-Control-Allow-Origin", "*")
+    ctx.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE")
     await next()
 }
