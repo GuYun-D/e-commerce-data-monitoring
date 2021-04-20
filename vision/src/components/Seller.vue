a1eef0d1805326994bf64c628227216ebda8944d
GuYun-D <1946306647@qq.com>
2021-04-20 19:02:37 +0800

    图标的动态刷新

--- vision/src/components/Seller.vue
+++ vision/src/components/Seller.vue
@@ -15,6 +15,12 @@ export default {
       chartInstance: null,
       // 服务器返回的数据
       allData: null,
+      // 当前显示的页数
+      currentPage: 1,
+      // 一共有多少页
+      totalPage: 0,
+      // 定时器id
+      timerID: null
     }
   },
 
@@ -22,22 +28,42 @@ export default {
     // 初始话echarts对象
     initChart() {
       this.chartInstance = this.$echarts.init(this.$refs.seller_ref)
+      // 对图标对象进行事件监听
+      this.chartInstance.on("mouseover", () => {
+        clearInterval(this.timerID)
+      })
+
+      this.chartInstance.on('mouseout', () => {
+        this.startInterval()
+      })
     },
     // 获取数据
     async getData() {
-      // http://localhost: 8080/api/seller
+      // http://localhost: 3000/api/seller
       const { data: ret } = await this.$http.get('/seller')
       console.log(ret)
       this.allData = ret
+      // 对数组排序
+      this.allData.sort(function (a, b) {
+        return a.value - b.value
+      })
+      // 每五个一页
+      this.totalPage = this.allData.length % 5 === 0 ? this.allData.length / 5 : this.allData.length + 1
       this.updataChart()
+      // 启动定时器，实时刷新
+      this.startInterval()
     },
     // 更新图表
     updataChart() {
-      const sellerNames = this.allData.map((item) => {
+      const start = (this.currentPage - 1) * 5 // 0
+      const end = this.currentPage * 5 // 5
+      const showData = this.allData.slice(start, end)
+
+      const sellerNames = showData.map((item) => {
         return item.name
       })
 
-      const sellerValues = this.allData.map((item) => {
+      const sellerValues = showData.map((item) => {
         return item.value
       })
 
@@ -46,12 +72,12 @@ export default {
           type: 'value',
         },
         yAxis: {
-          type: "category",
+          type: 'category',
           data: sellerNames,
         },
         series: [
           {
-            type: "bar",
+            type: 'bar',
             data: sellerValues,
           },
         ],
@@ -59,12 +85,31 @@ export default {
 
       this.chartInstance.setOption(option)
     },
+
+    startInterval() {
+      if(this.timerID){
+        clearInterval(this.timerID)
+      }
+      this.timerID = setInterval(() => {
+        this.currentPage ++
+        if(this.currentPage > this.totalPage){
+          this.currentPage = 1
+        }
+        this.updataChart()
+      }, 3000)
+    },
   },
 
+  // dom加载完毕的生命周期函数
   mounted() {
     this.initChart()
     this.getData()
   },
+
+  // 组件被销毁的生命周期
+  destoryed(){
+    clearInterval(timerID)
+  }
 }
 </script>
 
