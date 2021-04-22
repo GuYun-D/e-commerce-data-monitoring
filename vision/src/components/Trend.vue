4d2ababab571d196b4efd7258271fad0665587d4
GuYun-D <1946306647@qq.com>
2021-04-22 17:10:25 +0800

    完成适配

--- vision/src/components/Trend.vue
+++ vision/src/components/Trend.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="com-container">
-    <div class="title">
-      <span>{{showTitle}}</span>
-      <span class="iconfont" id="title-icon" @click="showChoice=!showChoice">&#xe6eb;</span>
-      <div class="selete_con" v-show="showChoice">
-        <div class="selete_item" v-for="item in seleteTypes" :key="item.key" @click="handleSelect(item.key)">{{item.text}}</div>
+    <div class="title" :style="comStyle">
+      <span>{{ '|      ' + showTitle }}</span>
+      <span :style="comStyle" class="iconfont" id="title-icon" @click="showChoice = !showChoice">&#xe6eb;</span>
+      <div class="selete_con" v-show="showChoice" :style="marginStyle">
+        <div class="selete_item" v-for="item in seleteTypes" :key="item.key" @click="handleSelect(item.key)">{{ item.text }}</div>
       </div>
     </div>
     <div class="com-chart" ref="trend_ref"></div>
@@ -20,7 +20,9 @@ export default {
       // 控制标题的显示与隐藏
       showChoice: false,
       // 显示数据的类型
-      choiceType: 'map'
+      choiceType: 'map',
+      // 字体大小
+      titleFontSize: 0,
     }
   },
 
@@ -126,16 +128,27 @@ export default {
     },
 
     screenAdapter() {
-      const adapteeOption = {}
+      // 根据宽度计算字体大小
+      this.titleFontSize = (this.$refs.trend_ref.offsetWidth / 100) * 3.6
+      const adapteeOption = {
+        legend: {
+          itemWidth: this.titleFontSize,
+          itemHeight: this.titleFontSize,
+          itemGap: this.titleFontSize,
+          textStyle: {
+            fontSize: this.titleFontSize / 2,
+          },
+        },
+      }
       this.chartsInstance.setOption(adapteeOption)
       this.chartsInstance.resize()
     },
 
-    handleSelect(currentType){
+    handleSelect(currentType) {
       this.choiceType = currentType
       this.updataChart()
       this.showChoice = false
-    }
+    },
   },
 
   destroyed() {
@@ -143,24 +156,38 @@ export default {
   },
 
   computed: {
-    seleteTypes(){
-      if(!this.allData){
+    seleteTypes() {
+      if (!this.allData) {
         return []
-      }else{
-        return this.allData.type.filter(item => {
+      } else {
+        return this.allData.type.filter((item) => {
           return item.key != this.choiceType
         })
       }
     },
 
-    showTitle(){
-      if(!this.allData){
+    showTitle() {
+      if (!this.allData) {
         return ''
-      }else{
+      } else {
         return this.allData[this.choiceType].title
       }
-    }
-  }
+    },
+
+    // 设置字体大小
+    comStyle() {
+      return {
+        fontSize: this.titleFontSize + 'px',
+      }
+    },
+
+    // 设置间距
+    marginStyle() {
+      return {
+        marginLeft: this.titleFontSize - 20 + 'px',
+      }
+    },
+  },
 }
 </script>
 
@@ -175,10 +202,16 @@ export default {
   #title-icon {
     margin-left: 10px;
     cursor: pointer;
+  }
 
-    .selete_item{
+  .selete_item {
+    &:hover {
       cursor: pointer;
     }
+  }
+
+  .selete_con {
+    background-color: #222733;
   }
 }
 </style>
