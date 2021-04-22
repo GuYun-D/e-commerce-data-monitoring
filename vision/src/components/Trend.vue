995f13ff57a9cebeb5593810c6ef06de4349c963
GuYun-D <1946306647@qq.com>
2021-04-22 16:09:08 +0800

    样式渲染

--- vision/src/components/Trend.vue
+++ vision/src/components/Trend.vue
@@ -22,14 +22,34 @@ export default {
 
   methods: {
     initCharts() {
-      this.chartsInstance = this.$echarts.init(this.$refs.trend_ref)
+      this.chartsInstance = this.$echarts.init(this.$refs.trend_ref, 'chalk')
       const initOption = {
         xAxis: {
           type: 'category',
+          // 紧挨边缘
+          boundaryGap: false,
         },
 
         yAxis: {
           type: 'value',
+        },
+
+        tooltip: {
+          trigger: 'axis',
+        },
+
+        legend: {
+          left: 20,
+          top: '15%',
+          icon: 'circle',
+        },
+
+        grid: {
+          top: '35%',
+          right: '4%',
+          bottom: '1%',
+          left: '3%',
+          containLabel: true,
         },
       }
       this.chartsInstance.setOption(initOption)
@@ -43,17 +63,46 @@ export default {
     },
 
     updataChart() {
+      // 定义颜色值
+      // 半透明的颜色值
+      const colorArr1 = [
+        'rgba(11, 168, 44, 0.5)',
+        'rgba(44, 110, 255, 0.5)',
+        'rgba(22, 242, 217, 0.5)',
+        'rgba(254, 33, 30, 0.5)',
+        'rgba(250, 105, 0, 0.5)'
+      ]
+      // 全透明的颜色值
+      const colorArr2 = [
+        'rgba(11, 168, 44, 0)',
+        'rgba(44, 110, 255, 0)',
+        'rgba(22, 242, 217, 0)',
+        'rgba(254, 33, 30, 0)',
+        'rgba(250, 105, 0, 0)'
+      ]
+
       // 获取类目轴数据
-      const timerArr = this.allData.common.moth
+      const timerArr = this.allData.common.month
       // y轴数据
       const valueArr = this.allData.map.data
       // 数据
-      const seriesArr = valueArr.map((item) => {
+      const seriesArr = valueArr.map((item, index) => {
         return {
           name: item.name,
           type: 'line',
           data: item.data,
-          stack: 'map'
+          stack: 'map',
+          areaStyle: {
+            color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [
+              {
+                offset: 0,
+                color: colorArr1[index]
+              },{
+                offset: 1,
+                color: colorArr2[index]
+              }
+            ])
+          }
         }
       })
       // 图例数据
@@ -63,7 +112,7 @@ export default {
       console.log(lengedArr)
 
       const dataOption = {
-        xAixs: {
+        xAxis: {
           data: timerArr,
         },
 
