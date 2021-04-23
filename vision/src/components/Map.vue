f00a014a55b1dfc362ce650687cb60676f4b1da0
GuYun-D <1946306647@qq.com>
2021-04-23 20:56:07 +0800

    完成地图点击事件

--- vision/src/components/Map.vue
+++ vision/src/components/Map.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="com-container">
-    <div class="com-chart" ref="map_ref">
-      
-    </div>
+  <div class="com-container" @dblclick="reverMap">
+    <div class="com-chart" ref="map_ref"></div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+// 导入工具方法，用于处理中————>英
+import { getProvinceMapInfo } from '../utils/map_utils'
 export default {
   data() {
     return {
       chartsInstance: null,
       allData: null,
+      mapStatus: false
     }
   },
   methods: {
@@ -28,7 +29,7 @@ export default {
         title: {
           text: '| 商家分布',
           left: 20,
-          top: 20
+          top: 20,
         },
         geo: {
           type: 'map',
@@ -37,17 +38,39 @@ export default {
           bottom: '5%',
           itemStyle: {
             areaColor: '#2e728f',
-            borderColor: '#333'
-          }
+            borderColor: '#333',
+          },
         },
         legend: {
           left: '5%',
           bottom: '5%',
           // 将图例排列方式改为竖直
-          orient: 'vertical'
-        }
+          orient: 'vertical',
+        },
       }
       this.chartsInstance.setOption(initOption)
+
+      // 点击事件的监听
+      // arg包含哪个省被点击的信息
+      this.chartsInstance.on('click', async (arg) => {
+        // console.log(arg)
+        // proovince包含省份的拼音以及地图地址
+        const proovince = getProvinceMapInfo(arg.name)
+        // console.log(proovince);
+        // 获取地图矢量数据
+        const res = await axios.get(`http://localhost:8080${proovince.path}`)
+        // console.log(res);
+        // 注册地图矢量数据
+        this.$echarts.registerMap(proovince.key, res.data)
+
+        // 切换地图
+        const changeOption = {
+          geo: {
+            map: proovince.key,
+          },
+        }
+        this.chartsInstance.setOption(changeOption)
+      })
     },
     async getData() {
       const { data: ret } = await this.$http.get('/map')
@@ -56,12 +79,12 @@ export default {
     },
     updataChart() {
       // 图例数据
-      const legendArr = this.allData.map(item => {
+      const legendArr = this.allData.map((item) => {
         return item.name
       })
 
       // 处理数据
-      const seriesArr = this.allData.map(item => {
+      const seriesArr = this.allData.map((item) => {
         // return的对象就是代表一个类别的所有散点数据
         // 类别：ret中三个数组，分别代表三个类别，黄金，白金，钻石
         // 如果想要在地图中显示散点数据，需要给散点图表添加配置：coordinateSystem: 'geo'
@@ -72,39 +95,50 @@ export default {
           coordinateSystem: 'geo',
           rippleEffect: {
             scale: 5,
-            brushType: 'stroke'
-          }
+            brushType: 'stroke',
+          },
         }
       })
 
       const dataOption = {
         series: seriesArr,
         legend: {
-          data: legendArr
-        }
+          data: legendArr,
+        },
       }
       this.chartsInstance.setOption(dataOption)
     },
     screenAdapter() {
-      const titleFontSize = this.$refs.map_ref.offsetWidth / 100 * 3.6
+      const titleFontSize = (this.$refs.map_ref.offsetWidth / 100) * 3.6
       const adapteeOption = {
         title: {
           textStyle: {
-            fontSize: titleFontSize
-          }
+            fontSize: titleFontSize,
+          },
         },
 
         legend: {
-          itemWidth : titleFontSize / 2,
-          itemHeight : titleFontSize / 2,
+          itemWidth: titleFontSize / 2,
+          itemHeight: titleFontSize / 2,
           itemGap: titleFontSize / 2,
           textStyle: {
-            fontSize: titleFontSize / 2
-          }
-        }
+            fontSize: titleFontSize / 2,
+          },
+        },
       }
       this.chartsInstance.setOption(adapteeOption)
       this.chartsInstance.resize()
+    },
+
+    // 回到中国地图
+    reverMap() {
+      const reverOption = {
+        geo: {
+          map: 'china',
+        },
+      }
+
+      this.chartsInstance.setOption(reverOption)
     },
   },
   mounted() {
