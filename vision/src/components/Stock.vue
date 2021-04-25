fd86d2cd7c08d73c955c82424e721deb56ba7427
GuYun-D <1946306647@qq.com>
2021-04-25 12:50:34 +0800

    样式渲染

--- vision/src/components/Stock.vue
+++ vision/src/components/Stock.vue
@@ -14,8 +14,13 @@ export default {
   },
   methods: {
     initCharts() {
-      this.chartsInstance = this.$echarts.init(this.$refs.stock_ref)
+      this.chartsInstance = this.$echarts.init(this.$refs.stock_ref, 'chalk')
       const initOption = {
+        title: {
+          text: '|   库存和销量分析',
+          top: 20,
+          left: 20,
+        },
       }
       this.chartsInstance.setOption(initOption)
     },
@@ -34,6 +39,16 @@ export default {
         ['34%', '75%'],
         ['66%', '75%'],
       ]
+
+      // 圆环图渐变色
+      const colorArr = [
+        ['#4ff778', '#0ba82c'],
+        ['#e5dd45', '#e8b11c'],
+        ['#e8821c', '#e55445'],
+        ['#5052ee', '#ab6ee5'],
+        ['#23e5e5', '#2e72bf'],
+      ]
+
       // 处理数据
       const showData = this.allData.slice(0, 5)
       const seriesArr = showData.map((item, index) => {
@@ -42,21 +57,46 @@ export default {
           // 变成圆环图
           radius: [110, 100],
           center: centerArr[index],
+          hoverAnimation: false,
+          labelLine: {
+            show: false,
+          },
+          label: {
+            position: 'center',
+            color: colorArr[index][0],
+          },
           data: [
             {
               // 销量
+              name: item.name + '\n' + item.sales,
               value: item.sales,
+              itemStyle: {
+                color: new this.$echarts.graphic.LinearGradient(0, 0, 1, 0, [
+                  {
+                    offset: 0,
+                    color: colorArr[index][0],
+                  },
+                  {
+                    offset: 1,
+                    color: colorArr[index][1],
+                  },
+                ]),
+              },
             },
             {
               // 库存
               value: item.stock,
+              // 库存的颜色都是灰色
+              itemStyle: {
+                color: '#333843',
+              },
             },
           ],
         }
       })
 
       const dataOption = {
-        series: seriesArr
+        series: seriesArr,
       }
       this.chartsInstance.setOption(dataOption)
     },
