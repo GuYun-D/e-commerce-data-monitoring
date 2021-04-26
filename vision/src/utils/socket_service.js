08c5291cf7dbc4769e5982589856e9fee343515f
GuYun-D <1946306647@qq.com>
2021-04-26 10:28:11 +0800

    存储回调函数

--- vision/src/utils/socket_service.js
+++ vision/src/utils/socket_service.js
@@ -15,6 +15,9 @@ export default class SocketService {
   // 和服务端连接的socket对象
   ws = null
 
+  // 存储回调函数
+  callbackMapping = {}
+
   // 定义链接服务器的代码
   connect() {
     // 连接服务器
@@ -37,7 +40,23 @@ export default class SocketService {
     this.ws.onmessage = msg => {
       console.log("服务端发送数据了");
       // 服务端发送过来的原始数据是在msg中的data
-      console.log(msg.data);
+      // console.log(msg.data);
+
+      /**
+       * 该方法得到后端传来的数据之后，要传给组件
+       * 事先将图表组件的某一个方法存储在当前这个模块中
+       * 一旦得到数据在调用此方法，将数据传给图表组件
+       */
     }
+  }
+
+  // 回调函数的注册
+  registerCallBack (socketType, callBack){
+    this.callbackMapping[socketType] = callBack
+  }
+
+  // 回调函数的取消
+  unregisterCallBack (socketType){
+    this.callbackMapping[socketType] = null
   }
 }
